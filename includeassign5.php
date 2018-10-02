61611ceb84268586693262002f73f56a7d607e26
KamahGit <kamahgenius@gmail.com>
2018-10-03 00:36:21 +0300

    include web page header,body footer assignment

---
+++ includeassign5.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruce
+ * Date: 10/2/2018
+ * Time: 4:59 PM
+ */
+include_once ('header.html');
+include_once ('body.html');
+require_once ('footer.html');
+echo "</html>";
