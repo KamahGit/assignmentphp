1d6fab417b56ed106fc90dfb83ff96621f8ee096
KamahGit <kamahgenius@gmail.com>
2018-10-03 08:41:33 +0300

    oop assignment modified

--- studentoop.php
+++ studentoop.php
@@ -11,15 +11,17 @@ class CytonnClass
 
 	
 
-	
+public function displayClass(){
+    echo "The ".$this->name." ".$this->type." runs for ".$this->length." hours. And has ".$this->students." students <br />";
+
+}
 }
 $sdt_class = new CytonnClass;
 $sdt_class->name = "Software Development";
 $sdt_class->length = 3;
 $sdt_class->students = 20;
 $sdt_class->type = "Evening Class";
-echo "The ".$sdt_class->name."".$sdt_class->type." runs for ".$sdt_class->length." hours. And has ".$sdt_class->students." students";
-
+$sdt_class->displayClass();
 
 /**
  * PROCEDURAL
@@ -28,7 +30,7 @@ $class_name="Software Development";
 $class_length = 3;
 $class_students = 20;
 $class_type = "Evening Class";
-echo "The ".$class_name."".$class_type." runs for ".$class_length." hours. And has ".$class_students." students";
+echo "The ".$class_name." ".$class_type." runs for ".$class_length." hours. And has ".$class_students." students";
 
 ?>
 <!DOCTYPE html>
@@ -44,6 +46,8 @@ echo "The ".$class_name."".$class_type." runs for ".$class_length." hours. And h
 <li>IN OOP, USE OF ARROWS ELIMINATES THE NEED TO STRONGLY/SPECIFICALLY NAME VARIABLES AS THE POINTER DIRECTS EASILY MEANING TWO CLASSES CAN HAVE SIMILARE VARIABLES FOR DIFFERENT PURPOSES</li>
 <li>IN OOP MUCH EMPHASIS IS GIVEN TO DATA ORGANIZATION EVEN BEFORE PROCESSING SO DATA CAN BE EASILY FORMATTED AND DEBUGGED</li>
 <LI>VARIABLES CAN BE CHANGED DIRECTLY FROM THE CLASS TO AFFECT NEW OBJECTS WHILE IN PROCEDURAL IT MAY MEAN REWRITTTING A LOT OF CODE</LI>
+    <li>USE OF <B>this</B> causes functions to be reusable</li>
+</ol>
 </body>
 </html>
 
